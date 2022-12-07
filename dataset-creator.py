8fe40e15f2de7d53766867ae14409537993ce0ad
Jet <woodforlol@gmail.com>
2022-12-07 20:21:43 +1100

    feat: connect to git

--- dataset-creator.py
+++ dataset-creator.py
@@ -3,32 +3,42 @@ Author: woodfor woodforlol@gmail.com
 Date: 2022-12-07 19:28:12
 '''
 import requests
+import os
+from dotenv import load_dotenv
 
 # Set the repository and authentication parameters
-repo = 'owner/repo'
-auth = ('username', 'password')
-
+load_dotenv()
+repo = os.environ['repo']
+access_token = os.environ['git_repo_token']
+auth = f"Bearer {access_token}"
 # Get the pull requests for the repository
-response = requests.get(f'https://api.github.com/repos/{repo}/pulls', auth=auth)
+headers = {
+    'Authorization': auth
+}
+response = requests.get(
+    f'https://api.github.com/repos/{repo}/pulls', headers=headers)
 
-# Parse the pull requests from the response
-pull_requests = response.json()
+print(response)
+# # Parse the pull requests from the response
+# pull_requests = response.json()
 
-print(pull_requests)
+
+# print(pull_requests)
+
 
 # For each pull request, get the comments
 # for pull_request in pull_requests:
 #   # Get the comments for the pull request
 #   response = requests.get(pull_request['comments_url'], auth=auth)
-  
+
 #   # Parse the comments from the response
 #   comments = response.json()
-  
+
 #   # Process the comments and add them to the dataset
 #   for comment in comments:
 #     # Check if the comment was made by your colleague
 #     is_from_colleague = comment['user']['login'] == 'colleague'
-    
+
 #     # Add the comment to the dataset
 #     dataset.append({
 #       'pull_request_id': pull_request['id'],
